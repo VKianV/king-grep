bfad4fbf5a2d1bd3e3a8c8e393a51c5d734872bc
Kian Ahmadi <otyyjhct@gmail.com>
2025-08-04 11:57:15 +0330

    Refactoring parse_config to return an instance of a Config struct

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 use std::{env, fs};
 
 fn main() {
-    let arguments: Vec<String> = env::args().collect();
-    let (query, file_path) = (&arguments[1], &arguments[2]);
+    let input: Vec<String> = env::args().collect();
+    let config = parse_config(&input);
 
-    dbg!(query, file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
-    dbg!(contents);
+    println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
