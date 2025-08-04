b7bbffc252b71d0aed45049d789812dcece4421a
Kian Ahmadi <otyyjhct@gmail.com>
2025-08-04 08:21:34 +0330

    Reading the contents of the file specified by the second argument: i have created a poem.txt file at the root of the project and made a mechanism to read the conents of the file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let arguments: Vec<String> = env::args().collect();
-    let (query, path) = (&arguments[1], &arguments[2]);
+    let (query, file_path) = (&arguments[1], &arguments[2]);
 
-    dbg!(query, path);
+    dbg!(query, file_path);
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    dbg!(contents);
 }
