dc90015d5243836414618a78886faa8899a084a5
Kian Ahmadi <otyyjhct@gmail.com>
2025-08-06 12:06:04 +0330

    Writing error messages to standard error instead of standard output using eprintln!

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -12,7 +12,7 @@ fn main() {
     println!("In file {}", config.file_path);
 
     if let Err(e) = king_grep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
