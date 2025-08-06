df4a772b77b4ad22ff0aa78174df1cc99babbed8
Kian Ahmadi <otyyjhct@gmail.com>
2025-08-06 11:18:21 +0330

    Checking for any value in an environment variable named IGNORE_CASE

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::fs;
+use std::{env, fs};
 
 pub struct Config {
     pub query: String,
@@ -16,7 +16,13 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
@@ -29,7 +35,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         search(&config.query, &contents)
     };
 
-    for line in search(&config.query, &contents) {
+    for line in results {
         println!("{line}");
     }
     Ok(())
