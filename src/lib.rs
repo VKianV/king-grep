cb0ea49e288f3cd4dedcc11c6e0a6f366cae4007
Kian Ahmadi <otyyjhct@gmail.com>
2025-08-06 11:13:35 +0330

    Calling either search or search_case_insensitive based on the value in config.ignore_case

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use std::fs;
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -21,6 +22,12 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
 
     for line in search(&config.query, &contents) {
         println!("{line}");
